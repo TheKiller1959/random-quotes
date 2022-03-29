7c919f4c0099a8c5db4f3e899ad8095efad3b07f
TheKiller1959 <thekiller1959yt@gmail.com>
2022-03-28 20:18:31 -0500

    Agregado el target para el botón 'Share in Twitter'

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -9,7 +9,7 @@ const Button = ({handle, color}) => {
         <button className={'btn'} onClick={handle} title='New Quote'><input className="img" type="image" src={img1} alt=""/></button>
       </div>
       <div>
-        <button className={'btn'} title='Share in Twitter'><a href="https://twitter.com/intent/tweet?text=Frases%20célebres%20para%20compartir%20en%20Twitter&url=https%3A%2F%2Frandomquotes-thekiller1959.netlify.app&hashtags=frases,autor,diaadia"><input className="img" type="image" src={img2} alt="" /></a></button>
+        <button className={'btn'} title='Share in Twitter'><a href="https://twitter.com/intent/tweet?text=Frases%20célebres%20para%20compartir%20en%20Twitter&url=https%3A%2F%2Frandomquotes-thekiller1959.netlify.app&hashtags=frases,autor,diaadia" target='_blank' rel='noopener noreferrer'><input className="img" type="image" src={img2} alt="" /></a></button>
       </div>
     </div>
   )
